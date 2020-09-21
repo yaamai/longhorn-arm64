8c6e31381b06b376253f8bfaaae65569fbb5f1ff
yaamai <ya_amai@yahoo.co.jp>
2020-09-21 08:23:05 +0000

    Add push

---
+++ push.sh
@@ -0,0 +1,4 @@
+docker push yaamai/longhorn-ui:latest || true
+docker push yaamai/longhorn-manager:latest || true
+docker push yaamai/longhorn-instance-manager:latest || true
+docker push yaamai/longhorn-engine:latest || true
